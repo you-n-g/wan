7c018f316c6360856436ef10db105b86bfac8063
zhupr <32754851+zprongl@users.noreply.github.com>
2020-07-06 22:01:36 +0800

    Update setup.py
    add author

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 setup(
+    author = 'zhupr',
+    author_email = 'zplongr@hotmail.com',
     name='want',
     version='0.0.1',
     packages=find_packages(),
