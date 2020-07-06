23fa7c5d231922a8d60a87c8a096d7960a2a36ce
BigTreei <970633424@qq.com>
2020-07-06 22:31:21 +0800

    Update Setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
+
 setup(
-    author = 'zhupr',
-    author_email = 'zplongr@hotmail.com',
-    name='wan',
-    version='0.0.1',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': ['wan=wan:run'],
-    },
-    install_requires=[
-        'loguru>=0.5.1',
-        'notifiers>=1.2.1',
-        'fire>=0.3.1',
-        'psutil>=5.7.0',
-        'iterfzf>=0.5.0.20.0',
-    ],
+	author='zhupr,gazh',
+	author_email='zplongr@hotmail.com,v-gazh@hotmail.com',
+	name='wan',
+	version='0.0.1',
+	packages=find_packages(),
+	entry_points={
+		'console_scripts': ['wan=wan:run'],
+	},
+	install_requires=[
+		'loguru>=0.5.1',
+		'notifiers>=1.2.1',
+		'fire>=0.3.1',
+		'psutil>=5.7.0',
+		'iterfzf>=0.5.0.20.0',
+	],
 )
