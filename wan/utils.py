78ff2e0b5b88f19963863eadee36a998517bb119
Young <afe.young@gmail.com>
2020-08-11 12:52:41 +0000

    Add proxy based on env and fix iterfzf error

--- wan/utils.py
+++ wan/utils.py
@@ -1,6 +1,8 @@
 import subprocess
 from iterfzf import iterfzf
 from loguru import logger
+import os
+import stat
 
 
 def iter_ps():
@@ -16,10 +18,16 @@ def get_pid_from_line(line):
 
 def get_pid_via_fzf(exact=True):
     try:
+        # FIXME: This is a bug from iterfzf. The fzf may not be executable
         selected_line = iterfzf(iter_ps(), multi=False, exact=exact)
     except PermissionError as e:
-        logger.error(f'Please make {e.filename} executable(e.g  `chmod a+x {e.filename}`).')
-        return None
+        try:
+            os.chmod(e.filename, os.stat(e.filename).st_mode |  stat.S_IEXEC)
+        except PermissionError:
+            logger.error(f'Please make {e.filename} executable(e.g  `chmod a+x {e.filename}`).')
+            return None
+        else:
+            selected_line = iterfzf(iter_ps(), multi=False, exact=exact)
     return get_pid_from_line(selected_line)
 
 
