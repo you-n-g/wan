7df398e51fd8abbfb3211a09099cc085414132a6
Young <afe.young@gmail.com>
2022-01-19 14:27:46 +0800

    remove all dependancy limitation

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-	author='zhupr,gazh,you_N_G',
-	author_email='zplongr@hotmail.com,v-gazh@hotmail.com',
-	name='wan',
-	version='0.0.2.dev0',
-	packages=find_packages(),
-	entry_points={
-		'console_scripts': ['wan=wan:run'],
-	},
-	install_requires=[
-		'loguru>=0.5.1',
-		'notifiers>=1.2.1',
-		'fire>=0.3.1',
-		'psutil>=5.7.0',
-		'iterfzf>=0.5.0.20.0',
-                "PyYAML",
-	],
+    author='zhupr,gazh,you_N_G',
+    author_email='zplongr@hotmail.com,v-gazh@hotmail.com',
+    name='wan',
+    version='0.0.2.dev0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['wan=wan:run'],
+    },
+    install_requires=[
+        'loguru',
+        'notifiers',
+        'fire',
+        'psutil',
+        'iterfzf',
+        "PyYAML",
+    ],
 )
